d675c36b330a638c9e345188a952f77b4f233479
Sara King <sara@sara-king.com>
2025-11-02 16:46:15 -0600

    WIP get by product api

--- apre-server/src/routes/reports/sales/index.js
+++ apre-server/src/routes/reports/sales/index.js
@@ -5,10 +5,10 @@
  * Description: Apre sales report API for the sales reports
  */
 
-'use strict';
+"use strict";
 
-const express = require('express');
-const { mongo } = require('../../../utils/mongo');
+const express = require("express");
+const { mongo } = require("../../../utils/mongo");
 
 const router = express.Router();
 
@@ -24,14 +24,14 @@ const router = express.Router();
  *  .then(response => response.json())
  *  .then(data => console.log(data));
  */
-router.get('/regions', (req, res, next) => {
+router.get("/regions", (req, res, next) => {
   try {
-    mongo (async db => {
-      const regions = await db.collection('sales').distinct('region');
+    mongo(async (db) => {
+      const regions = await db.collection("sales").distinct("region");
       res.send(regions);
     }, next);
   } catch (err) {
-    console.error('Error getting regions: ', err);
+    console.error("Error getting regions: ", err);
     next(err);
   }
 });
@@ -48,32 +48,51 @@ router.get('/regions', (req, res, next) => {
  *  .then(response => response.json())
  *  .then(data => console.log(data));
  */
-router.get('/regions/:region', (req, res, next) => {
+router.get("/regions/:region", (req, res, next) => {
   try {
-    mongo (async db => {
-      const salesReportByRegion = await db.collection('sales').aggregate([
-        { $match: { region: req.params.region } },
-        {
-          $group: {
-            _id: '$salesperson',
-            totalSales: { $sum: '$amount'}
-          }
-        },
-        {
-          $project: {
-            _id: 0,
-            salesperson: '$_id',
-            totalSales: 1
-          }
-        },
-        {
-          $sort: { salesperson: 1 }
-        }
-      ]).toArray();
+    mongo(async (db) => {
+      const salesReportByRegion = await db
+        .collection("sales")
+        .aggregate([
+          { $match: { region: req.params.region } },
+          {
+            $group: {
+              _id: "$salesperson",
+              totalSales: { $sum: "$amount" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              salesperson: "$_id",
+              totalSales: 1,
+            },
+          },
+          {
+            $sort: { salesperson: 1 },
+          },
+        ])
+        .toArray();
       res.send(salesReportByRegion);
     }, next);
   } catch (err) {
-    console.error('Error getting sales data for region: ', err);
+    console.error("Error getting sales data for region: ", err);
+    next(err);
+  }
+});
+
+router.get("/sales-by-product/:product?", (req, res, next) => {
+  try {
+    mongo(async (db) => {
+      const allSales = await db
+        .collection("sales")
+        .aggregate([{ $match: {} }])
+        .toArray();
+
+      console.log(allSales.length, "allsalesdata");
+      res.status(200).json(allSales);
+    }, next);
+  } catch (err) {
     next(err);
   }
 });
--- apre-server/test/routes/reports/sales/index.spec.js
+++ apre-server/test/routes/reports/sales/index.spec.js
@@ -6,60 +6,64 @@
  */
 
 // Require the modules
-const request = require('supertest');
-const app = require('../../../../src/app');
-const { mongo } = require('../../../../src/utils/mongo');
+const request = require("supertest");
+const app = require("../../../../src/app");
+const { mongo } = require("../../../../src/utils/mongo");
 
-jest.mock('../../../../src/utils/mongo');
+jest.mock("../../../../src/utils/mongo");
 
 // Test the sales report API
-describe('Apre Sales Report API - Regions', () => {
+describe("Apre Sales Report API - Regions", () => {
   beforeEach(() => {
     mongo.mockClear();
   });
 
   // Test the sales/regions endpoint
-  it('should fetch a list of distinct sales regions', async () => {
+  it("should fetch a list of distinct sales regions", async () => {
     mongo.mockImplementation(async (callback) => {
       const db = {
         collection: jest.fn().mockReturnThis(),
-        distinct: jest.fn().mockResolvedValue(['North', 'South', 'East', 'West'])
+        distinct: jest
+          .fn()
+          .mockResolvedValue(["North", "South", "East", "West"]),
       };
       await callback(db);
     });
 
-    const response = await request(app).get('/api/reports/sales/regions'); // Send a GET request to the sales/regions endpoint
+    const response = await request(app).get("/api/reports/sales/regions"); // Send a GET request to the sales/regions endpoint
 
     expect(response.status).toBe(200); // Expect a 200 status code
-    expect(response.body).toEqual(['North', 'South', 'East', 'West']); // Expect the response body to match the expected data
+    expect(response.body).toEqual(["North", "South", "East", "West"]); // Expect the response body to match the expected data
   });
 
   // Test the sales/regions endpoint with no regions found
-  it('should return 404 for an invalid endpoint', async () => {
-    const response = await request(app).get('/api/reports/sales/invalid-endpoint'); // Send a GET request to an invalid endpoint
+  it("should return 404 for an invalid endpoint", async () => {
+    const response = await request(app).get(
+      "/api/reports/sales/invalid-endpoint"
+    ); // Send a GET request to an invalid endpoint
     expect(response.status).toBe(404); // Expect a 404 status code
 
     // Expect the response body to match the expected data
     expect(response.body).toEqual({
-      message: 'Not Found',
+      message: "Not Found",
       status: 404,
-      type: 'error'
+      type: "error",
     });
   });
 
   // Test the sales/regions endpoint with no regions found
-  it('should return 200 with an empty array if no regions are found', async () => {
+  it("should return 200 with an empty array if no regions are found", async () => {
     // Mock the MongoDB implementation
     mongo.mockImplementation(async (callback) => {
       const db = {
         collection: jest.fn().mockReturnThis(),
-        distinct: jest.fn().mockResolvedValue([])
+        distinct: jest.fn().mockResolvedValue([]),
       };
       await callback(db);
     });
 
     // Make a request to the endpoint
-    const response = await request(app).get('/api/reports/sales/regions');
+    const response = await request(app).get("/api/reports/sales/regions");
 
     expect(response.status).toBe(200); // Expect a 200 status code
     expect(response.body).toEqual([]); // Expect the response body to match the expected data
@@ -67,13 +71,13 @@ describe('Apre Sales Report API - Regions', () => {
 });
 
 // Test the sales report API
-describe('Apre Sales Report API - Sales by Region', () => {
+describe("Apre Sales Report API - Sales by Region", () => {
   beforeEach(() => {
     mongo.mockClear();
   });
 
   // Test the sales/regions/:region endpoint
-  it('should fetch sales data for a specific region, grouped by salesperson', async () => {
+  it("should fetch sales data for a specific region, grouped by salesperson", async () => {
     mongo.mockImplementation(async (callback) => {
       // Mock the MongoDB collection
       const db = {
@@ -81,65 +85,82 @@ describe('Apre Sales Report API - Sales by Region', () => {
         aggregate: jest.fn().mockReturnValue({
           toArray: jest.fn().mockResolvedValue([
             {
-              salesperson: 'John Doe',
-              totalSales: 1000
+              salesperson: "John Doe",
+              totalSales: 1000,
             },
             {
-              salesperson: 'Jane Smith',
-              totalSales: 1500
-            }
-          ])
-        })
+              salesperson: "Jane Smith",
+              totalSales: 1500,
+            },
+          ]),
+        }),
       };
       await callback(db);
     });
 
-    const response = await request(app).get('/api/reports/sales/regions/north'); // Send a GET request to the sales/regions/:region endpoint
+    const response = await request(app).get("/api/reports/sales/regions/north"); // Send a GET request to the sales/regions/:region endpoint
     expect(response.status).toBe(200); // Expect a 200 status code
 
     // Expect the response body to match the expected data
     expect(response.body).toEqual([
       {
-        salesperson: 'John Doe',
-        totalSales: 1000
+        salesperson: "John Doe",
+        totalSales: 1000,
       },
       {
-        salesperson: 'Jane Smith',
-        totalSales: 1500
-      }
+        salesperson: "Jane Smith",
+        totalSales: 1500,
+      },
     ]);
   });
 
-  it('should return 200 and an empty array if no sales data is found for the region', async () => {
+  it("should return 200 and an empty array if no sales data is found for the region", async () => {
     // Mock the MongoDB implementation
     mongo.mockImplementation(async (callback) => {
       const db = {
         collection: jest.fn().mockReturnThis(),
         aggregate: jest.fn().mockReturnValue({
-          toArray: jest.fn().mockResolvedValue([])
-        })
+          toArray: jest.fn().mockResolvedValue([]),
+        }),
       };
       await callback(db);
     });
 
     // Make a request to the endpoint
-    const response = await request(app).get('/api/reports/sales/regions/unknown-region');
+    const response = await request(app).get(
+      "/api/reports/sales/regions/unknown-region"
+    );
 
     // Assert the response
     expect(response.status).toBe(200);
     expect(response.body).toEqual([]);
   });
 
-  it('should return 404 for an invalid endpoint', async () => {
+  it("should return 404 for an invalid endpoint", async () => {
     // Make a request to an invalid endpoint
-    const response = await request(app).get('/api/reports/sales/invalid-endpoint');
+    const response = await request(app).get(
+      "/api/reports/sales/invalid-endpoint"
+    );
 
     // Assert the response
     expect(response.status).toBe(404);
     expect(response.body).toEqual({
-      message: 'Not Found',
+      message: "Not Found",
       status: 404,
-      type: 'error'
+      type: "error",
     });
+  });
+});
+
+describe("Apre Sales Report API - Sales by Product", () => {
+  beforeEach(() => {
+    mongo.mockClear();
+  });
+  const URL = "/api/reports/sales/sales-by-product";
+
+  it("responds 200, application/json, and returns an array of sales by product data", async () => {
+    const res = await request(app).get(`${URL}`);
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
   });
 });
